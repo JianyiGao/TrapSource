b23e483a8f4e7a616124597493212c5cf8f6a2ef
Stephanie Giraldo <stephaniegiraldo@ufl.edu>
2017-10-27 16:08:10 -0400

    updated development.js file

--- config/env/development.js
+++ config/env/development.js
@@ -4,8 +4,11 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
-    options: {},
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://6a_username:6abunny@ds043057.mlab.com:43057/6a_db',
+    options: {
+      user: '',
+      pass: ''
+    },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
   },
