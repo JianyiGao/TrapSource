1d7c830c0f19bd562fc2965246660cf4aae82c38
Edan Canning <edancanning@ufl.edu>
2017-10-30 18:22:43 -0400

    buttons rendering

--- site/public/js/tree.js
+++ site/public/js/tree.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 		render(d.question);
 	});
 
+	function renderResources(resource) {}
+
 	function render(question) {
 		$question = $("#question");
 		$h2 = $question.find("h2");
@@ -12,13 +14,27 @@ $(document).ready(function() {
 		$h2.text(question.questionTitle);
 		$p.text(question.questionParagraph);
 
-		var buttons = [];
+		var props = [];
 
 		for (q in question.answers) {
-			button = "<a class='answer_button'>" + q + "</a>";
+			props.push(q);
+		}
+
+		for (var i = 0; i < props.length; i++) {
+			button = "<a class='answer_button'>" + props[i] + "</a>";
 			$button = $(button);
-			$button.on("click");
-			buttons.push($(button));
+			if (question.answers[props[i]].question != null) {
+				$button.on(
+					"click",
+					render.bind(null, question.answers[props[i]].question)
+				);
+			} else {
+				$button.on(
+					"click",
+					renderResources.bind(null, question.answers[props[i]])
+				);
+			}
+			$buttons.append($button);
 		}
 	}
 });
