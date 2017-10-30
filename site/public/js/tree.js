a7e9845d2b8759104d7ad38287c409fc62c26460
Edan Canning <edancanning@ufl.edu>
2017-10-30 18:57:00 -0400

    adding breadcrumbs

--- site/public/js/tree.js
+++ site/public/js/tree.js
@@ -1,40 +1,47 @@
 $(document).ready(function() {
 	$.getJSON("../data.json", function(d) {
-		render(d.question);
+		render(d);
 	});
 
-	function renderResources(resource) {}
+	function renderClosure(d) {
+		function renderResources(resource) {}
 
-	function render(question) {
-		$question = $("#question");
-		$h2 = $question.find("h2");
-		$p = $question.find("p");
-		$buttons = $question.find("#buttons");
+		function renderBreadCrumbs(questionTitle) {}
 
-		$h2.text(question.questionTitle);
-		$p.text(question.questionParagraph);
+		function render(question) {
+			$question = $("#question");
+			$h2 = $question.find("h2");
+			$p = $question.find("p");
+			$buttons = $question.find("#buttons");
 
-		var props = [];
+			$h2.text(question.questionTitle);
+			$p.text(question.questionParagraph);
 
-		for (q in question.answers) {
-			props.push(q);
-		}
-		$buttons.empty();
-		for (var i = 0; i < props.length; i++) {
-			button = "<div class='answer_button'>" + props[i] + "</div>";
-			$button = $(button);
-			if (question.answers[props[i]].question != null) {
-				$button.on(
-					"click",
-					render.bind(null, question.answers[props[i]].question)
-				);
-			} else {
-				$button.on(
-					"click",
-					renderResources.bind(null, question.answers[props[i]])
-				);
+			var props = [];
+
+			for (q in question.answers) {
+				props.push(q);
 			}
-			$buttons.append($button);
+			$buttons.empty();
+			for (var i = 0; i < props.length; i++) {
+				button = "<div class='answer_button'>" + props[i] + "</div>";
+				$button = $(button);
+				if (question.answers[props[i]].question != null) {
+					$button.on(
+						"click",
+						render.bind(null, question.answers[props[i]].question)
+					);
+				} else {
+					$button.on(
+						"click",
+						renderResources.bind(null, question.answers[props[i]])
+					);
+				}
+				$buttons.append($button);
+			}
+			renderBreadCrumbs(questions.questionTitle);
 		}
+
+		render(d.question);
 	}
 });
