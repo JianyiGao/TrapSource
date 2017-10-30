bcb2ad1062d8cd70815924e266b3ae63f0d5d6ad
Edan Canning <edancanning@ufl.edu>
2017-10-30 17:59:49 -0400

    starting work on tree

--- site/public/js/tree.js
+++ site/public/js/tree.js
@@ -1,1 +1,24 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+	$.getJSON("../data.json", function(d) {
+		render(d.question);
+	});
+
+	function render(question) {
+		$question = $("#question");
+		$h2 = $question.find("h2");
+		$p = $question.find("p");
+		$buttons = $question.find("#buttons");
+
+		$h2.text(question.questionTitle);
+		$p.text(question.questionParagraph);
+
+		var buttons = [];
+
+		for (q in question.answers) {
+			button = "<a class='answer_button'>" + q + "</a>";
+			$button = $(button);
+			$button.on("click");
+			buttons.push($(button));
+		}
+	}
+});
