3f5c737a5a2aaa490f5af47577e705d72e701fdc
Bao-Loc Trinh <spiritnumber10@gmail.com>
2017-11-08 13:23:25 -0500

    Fixed blank page due to adminpage error

--- modules/admins/client/config/admins.client.config.js
+++ modules/admins/client/config/admins.client.config.js
@@ -5,7 +5,7 @@
     .module('admins')
     .run(menuConfig);
 
-  menuConfig.$inject = ['menuService'];
+  menuConfig.$inject = ['Menus'];
 
   function menuConfig(menuService) {
     // Set top bar menu items
