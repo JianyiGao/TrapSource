adbdaed9f8bca9d4ab90857dcbd9b1fa77031e27
Stephanie Giraldo <stephaniegiraldo@ufl.edu>
2017-11-28 01:03:26 -0500

    moved js files from html to separate js

--- site/public/test/about.test.js
+++ site/public/test/about.test.js
@@ -1,4 +1,5 @@
-Qunit.test("Admin or Regular user", function(render){
+QUnit.test("admin or regular user", function(render){
     var userid = 'DaQoaYhJ7KW8ep4m4P0YLZUfcTk1';
-    render.equals()
+    render.equal(userid, 'DaQoaYhJ7KW8ep4m4P0YLZUfcTk1', "this is an admin user");
+    render.equal(userid, 'DaQoaYhJ7KW8ep4m4P0YLZUfcTk1', "this is a regular user");
 });
