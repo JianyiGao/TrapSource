0905f26818520d3722ac9f545fc97fde72cd1a73
Canning <edancanning@ufl.edu>
2017-11-30 14:07:00 -0500

    added test for admin mode injection on about page :

--- site/public/test/about.test.js
+++ site/public/test/about.test.js
@@ -18,3 +18,96 @@ QUnit.test("Test Firebase Initial Data Injection", function(assert) {
     done();
   });
 });
+
+QUnit.test("Test Admin Mode Injected Correctly", function(assert) {
+  var done = assert.async();
+  var database = firebase.database();
+  var data = {};
+  database.ref("test/about").once("value", function(snapshot) {
+    data = snapshot.val();
+    window.trapsourceTest.admin(snapshot);
+    var abtTitle = $("#abtTitle");
+    var abtDescription = $("#abtDescription");
+    var abtPar = $("#abtPar");
+    var submit_changes = $("#submit_changes");
+    assert.equal(
+      abtTitle.html(),
+      data.abtTitle,
+      "Admin title injected correctly"
+    );
+    assert.equal(
+      abtDescription.html(),
+      data.abtDescription,
+      "Admin description injected correctly"
+    );
+    assert.equal(
+      abtTitle.html(),
+      data.abtTitle,
+      "Admin title injected correctly"
+    );
+    assert.notEqual(
+      submit_changes.html(),
+      undefined,
+      "Admin save changes button exists"
+    );
+    done();
+  });
+});
+
+QUnit.test("Test Admin Mode Is Correctly Editable", function(assert) {
+  var done = assert.async();
+  var database = firebase.database();
+  var data = {};
+  database.ref("test/about").once("value", function(snapshot) {
+    data = snapshot.val();
+    window.trapsourceTest.admin(snapshot);
+    var abtTitle = $("#abtTitle");
+    var abtDescription = $("#abtDescription");
+    var abtPar = $("#abtPar");
+    assert.equal(
+      abtTitle.attr("contentEditable"),
+      "true",
+      "Admin title is correctly editable"
+    );
+    assert.equal(
+      abtDescription.attr("contentEditable"),
+      "true",
+      "Admin description is correctly editable"
+    );
+    assert.equal(
+      abtPar.attr("contentEditable"),
+      "true",
+      "Admin paragraph is correctly editable"
+    );
+    done();
+  });
+});
+
+QUnit.test("Test Firebase Submit works correctly", function(assert) {
+  var done = assert.async();
+  var database = firebase.database();
+  var data = {};
+  database.ref("test/about").once("value", function(snapshot) {
+    data = snapshot.val();
+    window.trapsourceTest.admin(snapshot);
+    var abtTitle = $("#abtTitle");
+    var abtDescription = $("#abtDescription");
+    var abtPar = $("#abtPar");
+    assert.equal(
+      abtTitle.attr("contentEditable"),
+      "true",
+      "Admin title is correctly editable"
+    );
+    assert.equal(
+      abtDescription.attr("contentEditable"),
+      "true",
+      "Admin description is correctly editable"
+    );
+    assert.equal(
+      abtPar.attr("contentEditable"),
+      "true",
+      "Admin paragraph is correctly editable"
+    );
+    done();
+  });
+});
