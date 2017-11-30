a246a684b4168e5be1e5fd8b1cec135c63ba77f6
Canning <edancanning@ufl.edu>
2017-11-30 14:43:22 -0500

    fixed first index page test

--- site/public/test/index.test.js
+++ site/public/test/index.test.js
@@ -2,19 +2,58 @@ QUnit.test("Test Firebase Initial Data Injection", function(assert) {
   var done = assert.async();
   var database = firebase.database();
   var data = {};
-  database.ref("test/about").once("value", function(snapshot) {
+  database.ref("test/index/index").once("value", function(snapshot) {
     data = snapshot.val();
     window.trapsourceTest.firebaseInit(snapshot);
-    var abtTitle = $("#abtTitle");
-    var abtDescription = $("#abtDescription");
-    var abtPar = $("#abtPar");
-    assert.equal(abtTitle.html(), data.abtTitle, "Title injected correctly");
+    var jmbTitle = $("#jmbTitle");
+    var jmbDescription = $("#jmbDescription");
+    var mainPar1 = $("#mainPar1");
+    var mainPar2 = $("#mainPar2");
+    var mainPar3 = $("#mainPar3");
+    var mainTitle1 = $("#mainTitle1");
+    var mainTitle2 = $("#mainTitle2");
+    var mainTitle3 = $("#mainTitle3");
     assert.equal(
-      abtDescription.html(),
-      data.abtDescription,
-      "Description injected correctly"
+      jmbTitle.html(),
+      data.jmbTitle,
+      "Jumbotron title injected correctly"
     );
-    assert.equal(abtPar.html(), data.abtPar, "Paragraph injected correctly");
+    assert.equal(
+      jmbDescription.html(),
+      data.jmbDescription,
+      "Jumbotron description injected correctly"
+    );
+    assert.equal(
+      mainTitle1.html(),
+      data.mainTitle1,
+      "Column title 1 injected correctly"
+    );
+    assert.equal(
+      mainTitle2.html(),
+      data.mainTitle2,
+      "Column title 2 injected correctly"
+    );
+    assert.equal(
+      mainTitle3.html(),
+      data.mainTitle3,
+      "Column title 3 injected correctly"
+    );
+    assert.equal(
+      mainPar1.html(),
+      data.mainPar1,
+      "Paragragh column 1 injected correctly"
+    );
+    assert.equal(
+      mainPar2.html(),
+      data.mainPar2,
+      "Paragragh column 2 injected correctly"
+    );
+    assert.equal(
+      mainPar3.html(),
+      data.mainPar3,
+      "Paragragh column 3 injected correctly"
+    );
+
     done();
   });
 });
@@ -23,7 +62,7 @@ QUnit.test("Test Admin Mode Injected Correctly", function(assert) {
   var done = assert.async();
   var database = firebase.database();
   var data = {};
-  database.ref("test/about").once("value", function(snapshot) {
+  database.ref("test/index/index").once("value", function(snapshot) {
     data = snapshot.val();
     window.trapsourceTest.admin(snapshot);
     var abtTitle = $("#abtTitle");
@@ -58,7 +97,7 @@ QUnit.test("Test Regular Mode Re-injected Correctly", function(assert) {
   var done = assert.async();
   var database = firebase.database();
   var data = {};
-  database.ref("test/about").once("value", function(snapshot) {
+  database.ref("test/index/index").once("value", function(snapshot) {
     data = snapshot.val();
     window.trapsourceTest.regular(snapshot);
     var abtTitle = $("#abtTitle");
@@ -93,7 +132,7 @@ QUnit.test("Test Admin Mode Is Correctly Editable", function(assert) {
   var done = assert.async();
   var database = firebase.database();
   var data = {};
-  database.ref("test/about").once("value", function(snapshot) {
+  database.ref("test/index/index").once("value", function(snapshot) {
     data = snapshot.val();
     window.trapsourceTest.admin(snapshot);
     var abtTitle = $("#abtTitle");
