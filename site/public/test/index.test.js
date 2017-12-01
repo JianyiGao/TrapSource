1f19d9d4efad489b36c2aeedeb884c0d3f5b4731
Stephanie Giraldo <stephaniegiraldo@ufl.edu>
2017-11-30 19:24:12 -0500

    created a test to check if Admin tab appears for different users

--- site/public/test/index.test.js
+++ site/public/test/index.test.js
@@ -249,3 +249,28 @@ QUnit.test("Test Regular Mode Re-injected Correctly", function(assert) {
     done();
   });
 });
+
+QUnit.test("Test User Restrictions", function(assert){
+    var adminTab = $("#admin-head");
+    var u = window.trapsourceTest.giveMeUser();
+        if (u) {
+            var name;
+            if (u.displayName) {
+                name = u.displayName;
+            } else {
+                name = u.email.substr(0, u.email.indexOf("@"));
+            }
+            $("#login-head")
+                .text(name)
+                .css("font-weight", "bold");
+            var userName = $("login-head");
+            assert.equal(userName.html(), name, "Correct Username is displayed");
+            if (u.uid === "DaQoaYhJ7KW8ep4m4P0YLZUfcTk1") {
+                assert.equal(adminTab.html(), "Admin", "Admin User: Admin tab correctly displayed");
+            } else {
+                assert.equal(adminTab.html(), undefined, "Regular User: Admin tab not displayed")
+            }
+        } else {
+            assert.equal(adminTab.html(), undefined, "No user profile detected");
+        }
+});
