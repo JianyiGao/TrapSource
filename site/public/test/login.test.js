b4b10d87f697c99e30c44cc4721ad5e4560473b3
Anthony Codella <codellaanthony@gmail.com>
2017-12-01 15:18:57 -0500

    added first login test

--- site/public/test/login.test.js
+++ site/public/test/login.test.js
@@ -3,3 +3,14 @@ $(document).ready(function() {
     console.log($("#admin-head").attr("href", "admin.test.html"));
   }, 100);
 });
+
+QUnit.test("Testing shake modal function", function(assert) {
+  window.trapsourceTest.showLoginForm();
+  window.trapsourceTest.shakeModal();
+  var alert = $($(".error.alert.alert-danger").get(0));
+  assert.equal(
+    alert.text(),
+    "Invalid email/password combination",
+    "Warning message rendered correctly"
+  );
+});
