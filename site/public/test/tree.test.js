db89a9c78141d4fd083c3e4ea7eb95ff9590e1e6
Canning <edancanning@ufl.edu>
2017-12-01 13:35:46 -0500

    fixed advance tree test to compate to main database instead of test for correctness

--- site/public/test/tree.test.js
+++ site/public/test/tree.test.js
@@ -1,25 +1,33 @@
 //tree.test.js
-QUnit.test("Test User Restrictions", function(assert){
-    var adminTab = $("#admin-head");
-    var u = window.trapsourceTest.giveMeUser();
-    if (u) {
-        var name;
-        if (u.displayName) {
-            name = u.displayName;
-        } else {
-            name = u.email.substr(0, u.email.indexOf("@"));
-        }
-        $("#login-head")
-            .text(name)
-            .css("font-weight", "bold");
-        if (u.uid === "DaQoaYhJ7KW8ep4m4P0YLZUfcTk1") {
-            assert.equal(adminTab.html(), "Admin", "Admin User: Admin tab correctly displayed");
-        } else {
-            assert.equal(adminTab.html(), undefined, "Regular User: Admin tab not displayed")
-        }
+QUnit.test("Test User Restrictions", function(assert) {
+  var adminTab = $("#admin-head");
+  var u = window.trapsourceTest.giveMeUser();
+  if (u) {
+    var name;
+    if (u.displayName) {
+      name = u.displayName;
     } else {
-        assert.equal(adminTab.html(), undefined, "No user profile detected");
+      name = u.email.substr(0, u.email.indexOf("@"));
     }
+    $("#login-head")
+      .text(name)
+      .css("font-weight", "bold");
+    if (u.uid === "DaQoaYhJ7KW8ep4m4P0YLZUfcTk1") {
+      assert.equal(
+        adminTab.html(),
+        "Admin",
+        "Admin User: Admin tab correctly displayed"
+      );
+    } else {
+      assert.equal(
+        adminTab.html(),
+        undefined,
+        "Regular User: Admin tab not displayed"
+      );
+    }
+  } else {
+    assert.equal(adminTab.html(), undefined, "No user profile detected");
+  }
 });
 
 //$("#container").each(function(){
@@ -28,23 +36,23 @@ QUnit.test("Test User Restrictions", function(assert){
 //$("#container").each(function(){
 //console.log($(this).find(("h2, p")).text())})
 
+QUnit.test("Test Advanced Tree Has Correct Data From Database", function(
+  assert
+) {
+  var done = assert.async();
+  var database = firebase.database();
+  var data = {};
+  database.ref("tree").once("value", function(snapshot) {
+    data = snapshot.val();
+    $("#tree-type").val("Advanced");
+    //window.trapsourceTest.firebaseInit(snapshot);
 
-QUnit.test("Test Advanced Tree Has Correct Data From Database", function (assert){
-
-       var done = assert.async();
-       var database = firebase.database();
-       var data = {};
-       database.ref("test").once("value", function(snapshot){
-             data = snapshot.val();
-             $("#tree-type").val('Advanced');
-             //window.trapsourceTest.firebaseInit(snapshot);
-            
-              //window.trapsourceTest.closure = closure();
-              // $("#tree-type").val('Advanced');
-            window.trapsourceTest.advanced();
-             var h2 = {};
-             var p = {};
-                /*
+    //window.trapsourceTest.closure = closure();
+    // $("#tree-type").val('Advanced');
+    window.trapsourceTest.advanced();
+    var h2 = {};
+    var p = {};
+    /*
              $("#container").each(function(index){
                  h2=$(this).find("h2").text();
                  console.log(index+": "+h2);
@@ -52,18 +60,28 @@ QUnit.test("Test Advanced Tree Has Correct Data From Database", function (assert
                  console.log(p);
             });
             */
-             $(".advanced-question-container").each(function(index){
-                 h2=$(this).find("h2").text();
-               //  console.log(index+": "+h2);
-                assert.equal(h2, data.tree[index].questionTitle, "Question Title Loaded correctly")
-                //console.log ((index+": "+data.tree[index].questionTitle));
-                p=$(this).find("p").text();
-                // console.log(index+": "+p);
-                 assert.equal(p, data.tree[index].questionParagraph, "Question Paragraph Loaded correctly")
-                // console.log ((index+": "+data.tree[index].questionParagraph));
-            });
-             done();
-       });
-
+    $(".advanced-question-container").each(function(index) {
+      h2 = $(this)
+        .find("h2")
+        .text();
+      //  console.log(index+": "+h2);
+      assert.equal(
+        h2,
+        data[index].questionTitle,
+        "Question Title Loaded correctly"
+      );
+      //console.log ((index+": "+data.tree[index].questionTitle));
+      p = $(this)
+        .find("p")
+        .text();
+      // console.log(index+": "+p);
+      assert.equal(
+        p,
+        data[index].questionParagraph,
+        "Question Paragraph Loaded correctly"
+      );
+      // console.log ((index+": "+data.tree[index].questionParagraph));
+    });
+    done();
+  });
 });
-
