82f341e920929bb8fde384c5baaefa1c9143e5f3
Bao-Loc Trinh <spiritnumber10@gmail.com>
2017-12-05 19:52:01 -0500

    Commented tests

--- site/public/test/admin.test.js
+++ site/public/test/admin.test.js
@@ -1,21 +1,26 @@
-$(document).ready(function() {
-  setTimeout(function() {
+/*
+******This tests the Admin page using QUnit tests******
+*/
+$(document).ready(function () {
+  setTimeout(function () {
     console.log($("#admin-head").attr("href", "admin.test.html"));
   }, 100);
 });
 
-$(document).ready(function() {
-  QUnit.test("Test That Sidepanel Correctly Renders", function(assert) {
+//Test side panel renders
+$(document).ready(function () {
+  QUnit.test("Test That Sidepanel Correctly Renders", function (assert) {
     var done = assert.async();
     var database = firebase.database();
     var data = {};
-    database.ref("tree").once("value", function(snapshot) {
+    database.ref("tree").once("value", function (snapshot) {
       data = snapshot.val();
       window.trapsourceTest.sidePanelRender(snapshot.val());
 
       var questions = $(".frm-question");
       console.log(questions);
-      $.each(questions, function(key, question) {
+      $.each(questions, function (key, question) {
+        //test if question titles render correctly
         assert.equal(
           $(question)
             .find("a")
@@ -23,6 +28,7 @@ $(document).ready(function() {
           data[key].questionTitle,
           "Question " + (key + 1) + " correctly renders"
         );
+        //test if for empty question titles
         assert.notEqual(
           $(question)
             .find("a")
@@ -35,19 +41,21 @@ $(document).ready(function() {
     });
   });
 
+  //import tree from database
   var database = firebase.database();
-  database.ref("tree").once("value", function(snapshot) {
+  database.ref("tree").once("value", function (snapshot) {
     var data = snapshot.val();
     testRenderCorrecty(data);
   });
 
+  //test if questions correctly render into forms
   function testRenderCorrecty(data) {
     var i = 0;
     window.trapsourceTest.render(data, i, test);
     function test() {
       QUnit.test(
         "Test That the Form Num " + (i + 1) + " Rendered Correctly",
-        function(assert) {
+        function (assert) {
           var done = assert.async();
           var inputs = $(".frm-submit");
           var answerCount = 0;
@@ -55,21 +63,24 @@ $(document).ready(function() {
           var resourceCount = 0;
           var index = i;
           console.log(inputs);
-          $.each(inputs, function(key, input) {
+          $.each(inputs, function (key, input) {
             var $input = $(input);
             if (key === 0) {
+              //test of first question correctly renders in form
               assert.equal(
                 $input.val(),
                 data[index].questionTitle,
                 "Title correctly renders"
               );
             } else if (key === 1) {
+              //test if second question correctly renders in form
               assert.equal(
                 $input.val(),
                 data[index].questionParagraph,
                 "Description correctly renders"
               );
             } else if ($input.attr("id") === "Answer") {
+              //test if the rest of the questions correctly rener in form
               if ($(inputs.get(key + 1)).attr("id") === "Answer") {
                 assert.equal(
                   $input.val(),
@@ -78,6 +89,7 @@ $(document).ready(function() {
                 );
                 answerCount++;
               } else {
+                //test if nextBool works with path option
                 assert.equal(
                   $input.val(),
                   data[index].answers[answerCount].answerTitle,
@@ -86,37 +98,41 @@ $(document).ready(function() {
                 pathCount++;
               }
             } else if ($input.attr("id") === "Resource-Title") {
+              //test if number of resources for each question correctly render in form
               assert.equal(
                 $input.val(),
                 data[index].answers[answerCount].resourceTitle,
-                "Resoruce " + (resourceCount + 1) + " title correctly rendered"
+                "Resource " + (resourceCount + 1) + " title correctly rendered"
               );
             } else if ($input.attr("id") === "Resource-Paragraph") {
+              //test if resources for each questions correctly render in form
               assert.equal(
                 $input.val(),
                 data[index].answers[answerCount].resourceParagraph,
-                "Resoruce " +
-                  (resourceCount + 1) +
-                  " description correctly rendered"
+                "Resource " +
+                (resourceCount + 1) +
+                " description correctly rendered"
               );
             } else if ($input.attr("id") === "Resource-Name") {
+              //test if number of links for each resources correctly render in form
               assert.equal(
                 $input.val(),
                 data[index].answers[answerCount].resourceLinks[resourceCount]
                   .linkName,
-                "Resoruce " +
-                  (resourceCount + 1) +
-                  " link name correctly rendered"
+                "Resource " +
+                (resourceCount + 1) +
+                " link name correctly rendered"
               );
             } else if ($input.attr("id") === "Resource-URL") {
+              //test if links for each resources correctly render in form
               if ($(inputs.get(key + 1)).attr("id") !== "Resource-Name") {
                 assert.equal(
                   $input.val(),
                   data[index].answers[answerCount].resourceLinks[resourceCount]
                     .url,
                   "Resoruce " +
-                    (resourceCount + 1) +
-                    " link URL correctly rendered"
+                  (resourceCount + 1) +
+                  " link URL correctly rendered"
                 );
                 answerCount++;
                 resourceCount = 0;
@@ -126,8 +142,8 @@ $(document).ready(function() {
                   data[index].answers[answerCount].resourceLinks[resourceCount]
                     .url,
                   "Resoruce " +
-                    (resourceCount + 1) +
-                    " link URL correctly rendered"
+                  (resourceCount + 1) +
+                  " link URL correctly rendered"
                 );
                 resourceCount++;
               }
@@ -138,7 +154,7 @@ $(document).ready(function() {
             window.trapsourceTest.render(data, ++i, test);
           } else {
             var database = firebase.database();
-            database.ref("tree").once("value", function(snapshot) {
+            database.ref("tree").once("value", function (snapshot) {
               var data = snapshot.val();
               testNotEmpty(data);
             });
@@ -148,13 +164,14 @@ $(document).ready(function() {
     }
   }
 
+  //Test for empty fields
   function testNotEmpty(data) {
     var i = 0;
     window.trapsourceTest.render(data, i, test);
     function test() {
       QUnit.test(
         "Test That the Form Num " + (i + 1) + " has no empty values",
-        function(assert) {
+        function (assert) {
           var done = assert.async();
           var inputs = $(".frm-submit");
           var answerCount = 0;
@@ -162,9 +179,10 @@ $(document).ready(function() {
           var resourceCount = 0;
           var index = i;
           console.log(inputs);
-          $.each(inputs, function(key, input) {
+          $.each(inputs, function (key, input) {
             var $input = $(input);
             if (key === 0) {
+              //test if title is empty
               assert.notEqual($input.val(), "", "Title correctly renders");
             } else if (key === 1) {
               assert.notEqual(
@@ -173,6 +191,7 @@ $(document).ready(function() {
                 "Description correctly renders"
               );
             } else if ($input.attr("id") === "Answer") {
+              //test if path answer is empty
               if ($(inputs.get(key + 1)).attr("id") === "Answer") {
                 assert.notEqual(
                   $input.val(),
@@ -181,6 +200,7 @@ $(document).ready(function() {
                 );
                 answerCount++;
               } else {
+                //test if answers with no path is empty
                 assert.notEqual(
                   $input.val(),
                   "",
@@ -189,35 +209,39 @@ $(document).ready(function() {
                 pathCount++;
               }
             } else if ($input.attr("id") === "Resource-Title") {
+              //test if source titles are empty
               assert.notEqual(
                 $input.val(),
                 "",
-                "Resoruce " + (resourceCount + 1) + " title correctly rendered"
+                "Resource " + (resourceCount + 1) + " title correctly rendered"
               );
             } else if ($input.attr("id") === "Resource-Paragraph") {
+              //test if resource paragraphs are empty
               assert.notEqual(
                 $input.val(),
                 "",
-                "Resoruce " +
-                  (resourceCount + 1) +
-                  " description correctly rendered"
+                "Resource " +
+                (resourceCount + 1) +
+                " description correctly rendered"
               );
             } else if ($input.attr("id") === "Resource-Name") {
+              //test if links are empty
               assert.notEqual(
                 $input.val(),
                 "",
                 "Resoruce " +
-                  (resourceCount + 1) +
-                  " link name correctly rendered"
+                (resourceCount + 1) +
+                " link name correctly rendered"
               );
             } else if ($input.attr("id") === "Resource-URL") {
               if ($(inputs.get(key + 1)).attr("id") !== "Resource-Name") {
+                //test if urls are empty
                 assert.notEqual(
                   $input.val(),
                   "",
                   "Resoruce " +
-                    (resourceCount + 1) +
-                    " link URL correctly rendered"
+                  (resourceCount + 1) +
+                  " link URL correctly rendered"
                 );
                 answerCount++;
                 resourceCount = 0;
@@ -226,8 +250,8 @@ $(document).ready(function() {
                   $input.val(),
                   "",
                   "Resoruce " +
-                    (resourceCount + 1) +
-                    " link URL correctly rendered"
+                  (resourceCount + 1) +
+                  " link URL correctly rendered"
                 );
                 resourceCount++;
               }
