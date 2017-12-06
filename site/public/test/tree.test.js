ecb391bfb8a1c09a3a32dac23a7c2de61b6fab71
Edan Canning <edancanning@ufl.edu>
2017-12-05 23:50:03 -0500

    fix user test

--- site/public/test/tree.test.js
+++ site/public/test/tree.test.js
@@ -4,38 +4,6 @@ $(document).ready(function() {
   }, 100);
 });
 
-//tree.test.js
-QUnit.test('Test User Restrictions', function(assert) {
-  var adminTab = $('#admin-head');
-  var u = window.trapsourceTest.giveMeUser();
-  if (u) {
-    var name;
-    if (u.displayName) {
-      name = u.displayName;
-    } else {
-      name = u.email.substr(0, u.email.indexOf('@'));
-    }
-    $('#login-head')
-      .text(name)
-      .css('font-weight', 'bold');
-    if (u.uid === 'DaQoaYhJ7KW8ep4m4P0YLZUfcTk1') {
-      assert.equal(
-        adminTab.html(),
-        'Admin',
-        'Admin User: Admin tab correctly displayed'
-      );
-    } else {
-      assert.equal(
-        adminTab.html(),
-        undefined,
-        'Regular User: Admin tab not displayed'
-      );
-    }
-  } else {
-    assert.equal(adminTab.html(), undefined, 'No user profile detected');
-  }
-});
-
 //$("#container").each(function(){
 //console.log($(this).html())})
 
@@ -144,5 +112,36 @@ function advancedTreeTest() {
       });
       done();
     });
+  });
+  //tree.test.js
+  QUnit.test('Test User Restrictions', function(assert) {
+    var adminTab = $('#admin-head');
+    var u = window.trapsourceTest.giveMeUser();
+    if (u) {
+      var name;
+      if (u.displayName) {
+        name = u.displayName;
+      } else {
+        name = u.email.substr(0, u.email.indexOf('@'));
+      }
+      $('#login-head')
+        .text(name)
+        .css('font-weight', 'bold');
+      if (u.uid === 'DaQoaYhJ7KW8ep4m4P0YLZUfcTk1') {
+        assert.equal(
+          adminTab.html(),
+          'Admin',
+          'Admin User: Admin tab correctly displayed'
+        );
+      } else {
+        assert.equal(
+          adminTab.html(),
+          undefined,
+          'Regular User: Admin tab not displayed'
+        );
+      }
+    } else {
+      assert.equal(adminTab.html(), undefined, 'No user profile detected');
+    }
   });
 }
